b5e3f9a77e99402262bab220b946ea209a4ede96
Roger Melich <rogermelich@gmail.com>
2017-04-18 13:39:49 +0200

    Ok forms

--- src/Errors.js
+++ src/Errors.js
@@ -1,52 +1,101 @@
-export class Errors {
+export default class Errors {
     /**
-     *  Constructor
+     * Constructor.
+     *
      */
-    constructor() {
+    constructor () {
         this.errors = {}
     }
 
     /**
-     * API
+     * Determine if we have any errors.
+     *
+     * @returns {boolean}
      */
-    has(field) {
-        return this.errors.hasOwnProperty(field)
-    }
-
-    any() {
+    any () {
         return Object.keys(this.errors).length > 0
     }
 
     /**
-     * Retrieve the error message for a field
+     * Determine if we have any errors.
+     *
+     * @returns {boolean}
+     */
+    hasErrors () {
+        return this.any()
+    }
+
+    /**
+     * Get all of the raw errors for the collection.
+     *
+     * @returns {{}|*}
+     */
+    all () {
+        return this.errors
+    }
+
+    /**
+     * Check if exists error for a current field.
+     *
+     * @param field
+     * @returns {boolean}
+     */
+    has (field) {
+        return this.errors.hasOwnProperty(field)
+    }
+
+    /**
+     * Retrieve the error message for a field.
      *
      * @param field
      * @returns {*}
      */
-    get(field) {
-        if (this.errors[field]) {
+    get (field) {
+        if (this.has(field)) {
             return this.errors[field][0]
         }
-
     }
 
     /**
+     * Record the new errors.
      *
      * @param errors
      */
-    record(errors) {
+    record (errors) {
+        this.set(errors)
+    }
+
+    /**
+     * Set the raw errors for the collection.
+     *
+     * @param errors
+     */
+    set (errors) {
         this.errors = errors
     }
 
     /**
+     * Clear one or all error fields.
      *
      * @param field
      */
-    clear(field) {
+    clear (field) {
         if (field) {
-            delete this.errors(field)
+            delete this.errors[field]
+
             return
         }
+
         this.errors = {}
+    }
+
+    /**
+     * Clear all errors if no field parameter is provided
+     * or clear only field if provided.
+     *
+     * @param field
+     */
+    forget (field) {
+        this.clear(field)
     }
 }
--- src/Forms.js
+++ src/Forms.js
@@ -1,55 +1,225 @@
-export class From {
-    /**
-     *  Constructor
-     */
-    constructor(originalFields) {
-        this.fields = originalFields;
+import Errors from './Errors'
 
-        for (let field in originalFields) {
-            this[field] = originalFields[field]
-        }
+import axios from 'axios'
+
+export default class Form {
+    /**
+     * Constructor.
+     *
+     * @param fields
+     */
+    constructor (fields) {
+        this.clearOnSubmit = false
+
+        this.originalFields = fields
 
         this.errors = new Errors()
+
+        this.resetStatus()
+
+        for (let field in fields) {
+            this[field] = fields[field]
+        }
     }
 
     /**
-     * Reset
+     * Retrieve the field form.
+     *
+     * @param field
+     * @returns {*}
      */
-    reset() {
+    get (field) {
+        if (this.has(field)) {
+            return this[field]
+        }
+    }
+
+    /**
+     * Set the field value.
+     *
+     * @param field
+     * @param value
+     */
+    set (field, value) {
+        if (this.has(field)) {
+            this[field] = value
+        }
+    }
+
+    /**
+     * Check if a field exists on form
+     *
+     * @param field
+     * @returns {boolean}
+     */
+    has (field) {
+        return this.hasOwnProperty(field)
+    }
+
+    /**
+     * Reset form.
+     *
+     */
+    reset () {
         this.fields = {}
 
-        for (let field in originalFields) {
-            this.field = ''
+        for (let field in this.originalFields) {
+            this[field] = ''
         }
 
         this.errors.clear()
     }
 
     /**
-     * Submit
+     * Activates form clearing/reset after submit.
+     *
      */
-    submit(requiestType, url) {
-        return new Promise( (resolve,reject) => {
-            axios.post(url.this.fields)
+    clearOnSubmit () {
+        this.clearOnSubmit = true
+    }
+
+    /**
+     * Reset status.
+     *
+     */
+    resetStatus () {
+        this.errors.forget()
+        this.submitting = false
+        this.submitted = false
+        this.succeeded = false
+    }
+
+    /**
+     * Get form data.
+     *
+     * @returns {{}}
+     */
+    data () {
+        let data = {}
+
+        for (let field in this.originalFields) {
+            data[field] = this[field]
+        }
+
+        return data
+    }
+
+    /**
+     * Start processing the form.
+     *
+     */
+    startProcessing () {
+        this.errors.forget()
+        this.submitting = true
+        this.succeeded = false
+    };
+
+    /**
+     * Finish processing the form.
+     *
+     */
+    finishProcessing () {
+        this.submitting = false
+        this.submitted = false
+        this.succeeded = true
+    }
+
+    /**
+     * Finish processing the form on errors.
+     */
+    finishProcessingOnErrors () {
+        this.submitting = false
+        this.submitted = false
+        this.succeeded = false
+    }
+
+    /**
+     * Send a POST request to the given URL.
+     *
+     * @param url
+     * @returns {*}
+     */
+    post (url) {
+        return this.submit('post', url)
+    }
+
+    /**
+     * Send a PUT request to the given URL.
+     *
+     * @param url
+     * @returns {*}
+     */
+    put (url) {
+        return this.submit('put', url)
+    }
+
+    /**
+     * Send a PATCH request to the given URL.
+     *
+     * @param url
+     * @returns {*}
+     */
+    patch (url) {
+        return this.submit('patch', url)
+    }
+
+    /**
+     * Send a DELETE request to the given URL.
+     *
+     * @param url
+     * @returns {*}
+     */
+    delete (url) {
+        return this.submit('delete', url)
+    }
+
+    /**
+     * Submit the form to the back-end api/server.
+     *
+     * @param requesType
+     * @param url
+     * @returns {Promise}
+     */
+    submit (requesType, url) {
+        this.startProcessing()
+        return new Promise((resolve, reject) => {
+            axios[requesType](url, this.data())
                 .then(response => {
-                    console.log(responsive)
+                    this.onSuccess()
+                    resolve(response)
                 })
                 .catch(error => {
-                    reject(error.response.data)
+                    this.onFail(error.response.data)
+                    reject(error)
                 })
         })
     }
 
-    data(){}
-
-    onSuccess(data){
-        //TODO
-        this.reset()
+    /**
+     * Process on success.
+     */
+    onSuccess () {
+        this.finishProcessing()
+        if (this.clearOnSubmit) this.reset()
     }
 
-    onFail(data){
-        //TODO
-        this.errors.record(errors.response.data)
+    /**
+     * Process on fail.
+     *
+     * @param errors
+     */
+    onFail (errors) {
+        this.errors.record(errors)
+        this.finishProcessingOnErrors()
     }
 
+    /**
+     * Set the errors on the form.
+     *
+     * @param errors
+     */
+    setErrors (errors) {
+        this.submitting = false
+        this.errors.set(errors)
+    };
 }
